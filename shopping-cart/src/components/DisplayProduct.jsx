e1ab2ed9c031643d123433446d24d331b00e56c1
DanielMarchenaM <danielmm0596@gmail.com>
2026-02-28 19:45:19 -0600

    Display products' data in a component

---
+++ shopping-cart/src/components/DisplayProduct.jsx
@@ -0,0 +1,22 @@
+export default function DisplayProduct({ product }) {
+
+    const { id, name, image, description, price } = product
+    return (
+        <>
+            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div class="col-4">
+                    <img class="img-fluid" src={`./img/${image}.jpg`} alt="imagen guitarra" />
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p class="fw-black text-primary fs-3">${price}</p>
+                    <button
+                        type="button"
+                        class="btn btn-dark w-100"
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+        </>
+    )
+}
